a98e15a1da7a4b622972cf251e0adf19eed2e1dd
Ganeshrockz <ganeshrockz@github.com>
2022-03-27 22:01:00 +0530

    Updates

---
+++ precache-manifest.af607cdb00a12fd0176ee1a43473e8a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f0d68f883d78651dbe3",
+    "url": "/IPL-22/static/css/main.31e8e36b.chunk.css"
+  },
+  {
+    "revision": "5f0d68f883d78651dbe3",
+    "url": "/IPL-22/static/js/main.5f0d68f8.chunk.js"
+  },
+  {
+    "revision": "5b7d961161e13dda97d8",
+    "url": "/IPL-22/static/js/runtime~main.5b7d9611.js"
+  },
+  {
+    "revision": "0be92d95ed26d5569b46",
+    "url": "/IPL-22/static/css/2.801b82c9.chunk.css"
+  },
+  {
+    "revision": "0be92d95ed26d5569b46",
+    "url": "/IPL-22/static/js/2.0be92d95.chunk.js"
+  },
+  {
+    "revision": "9aa6f641275e775ca81cc515bd5ca0e7",
+    "url": "/IPL-22/index.html"
+  }
+];
